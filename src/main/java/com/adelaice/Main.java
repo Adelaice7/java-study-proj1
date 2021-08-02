4106100aeae2b05fb29837489f91378624446150
Ramona Meunier <gramyy7@gmail.com>
2021-08-02 17:00:38 +0200

    OOP/Polymorphism practice

--- src/main/java/com/adelaice/Main.java
+++ src/main/java/com/adelaice/Main.java
@@ -1,36 +1,99 @@
 package com.adelaice;
 
-import com.adelaice.model.*;
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here...";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet Earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze.";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial forces try to take over the universe.";
+    }
+}
+
+class ForgettableMovie extends Movie {
+    public ForgettableMovie() {
+        super("Forgettable Movie");
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + ": "
+                    + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
 
-//        Player player = new Player();
-//
-//        player.fullName = "Joe";
-////        player.health = 20;
-//        player.weapon = "Sword";
-//
-//        int damage = 10;
-//
-//        player.loseHealth(damage);
-//        System.out.println(player.healthRemaining());
-//
-//        damage = 11;
-//        player.health = 200;
-//        player.loseHealth(damage);
-//        System.out.println(player.healthRemaining());
+        }
+    }
 
-//        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Jack", "Sword", 50);
-//        System.out.println("Initial health = " + enhancedPlayer.getHealth());
-//        enhancedPlayer.loseHealth(40);
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number: " + randomNumber);
 
-        Printer printer = new Printer(100, true);
-
-        printer.fillUpToner(50);
-        printer.printPage(7);
-        printer.fillUpToner(100);
-
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new ForgettableMovie();
+            default:
+                return null;
+        }
     }
 }
