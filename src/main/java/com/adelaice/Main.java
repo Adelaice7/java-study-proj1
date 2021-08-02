2c01ad2a2ab79b0349f1f7de1f2c519143dbb1a7
Ramona Meunier <gramyy7@gmail.com>
2021-08-02 17:09:19 +0200

    OOP/Polymorphism practice 2

--- src/main/java/com/adelaice/Main.java
+++ src/main/java/com/adelaice/Main.java
@@ -1,67 +1,69 @@
 package com.adelaice;
 
-class Movie {
+class Car {
     private String name;
+    private int wheels;
+    private int doors;
+    private int cylinders;
 
-    public Movie(String name) {
+    public Car(String name, int doors, int cylinders) {
+        this.wheels = 4;
         this.name = name;
+        this.doors = doors;
+        this.cylinders = cylinders;
     }
 
-    public String plot() {
-        return "No plot here...";
+    public String startEngine() {
+        return "Starting engine of car";
     }
 
     public String getName() {
         return name;
     }
-}
 
-class Jaws extends Movie {
-    public Jaws() {
-        super("Jaws");
+    public int getWheels() {
+        return wheels;
     }
 
-    public String plot() {
-        return "A shark eats lots of people";
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getCylinders() {
+        return cylinders;
     }
 }
 
-class IndependenceDay extends Movie {
-    public IndependenceDay() {
-        super("Independence Day");
+class Convertible extends Car {
+    public Convertible() {
+        super("Convertible", 4, 4);
     }
 
     @Override
-    public String plot() {
-        return "Aliens attempt to take over planet Earth";
+    public String startEngine() {
+        return "Starting engine of Convertible...";
     }
 }
 
-class MazeRunner extends Movie {
-    public MazeRunner() {
-        super("Maze Runner");
+class Sedan extends Car {
+    public Sedan() {
+        super("Sedan", 4, 4);
     }
 
     @Override
-    public String plot() {
-        return "Kids try and escape a maze.";
+    public String startEngine() {
+        return "Starting engine of Sedan...";
     }
 }
 
-class StarWars extends Movie {
-    public StarWars() {
-        super("Star Wars");
+class Truck extends Car {
+    public Truck() {
+        super("Truck", 2, 5);
     }
 
     @Override
-    public String plot() {
-        return "Imperial forces try to take over the universe.";
-    }
-}
-
-class ForgettableMovie extends Movie {
-    public ForgettableMovie() {
-        super("Forgettable Movie");
+    public String startEngine() {
+        return "Starting engine of Truck...";
     }
 }
 
@@ -69,29 +71,25 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
-            Movie movie = randomMovie();
-            System.out.println("Movie #" + i + ": "
-                    + movie.getName() + "\n" +
-                    "Plot: " + movie.plot() + "\n");
+            Car car = randomCar();
+            System.out.println("Car #" + i + ": "
+                    + car.getName() + "\n" +
+                    "Plot: " + car.startEngine() + "\n");
 
         }
     }
 
-    public static Movie randomMovie() {
-        int randomNumber = (int) (Math.random() * 5) + 1;
+    public static Car randomCar() {
+        int randomNumber = (int) (Math.random() * 3) + 1;
         System.out.println("Random number: " + randomNumber);
 
         switch (randomNumber) {
             case 1:
-                return new Jaws();
+                return new Convertible();
             case 2:
-                return new IndependenceDay();
+                return new Sedan();
             case 3:
-                return new MazeRunner();
-            case 4:
-                return new StarWars();
-            case 5:
-                return new ForgettableMovie();
+                return new Truck();
             default:
                 return null;
         }
